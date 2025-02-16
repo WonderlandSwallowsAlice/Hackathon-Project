c4e16a98604df6e123a956d7fd6714d3aa1cde71
potapiva000 <ivan_potapov@msn.com>
2025-02-15 18:18:27 -0800

    Update TurtleDrawing.py

--- TurtleDrawing.py
+++ TurtleDrawing.py
@@ -1,3 +1,4 @@
+
 import turtle
 
 # gather user inputs for height, width, background color, and turtle/line color
@@ -9,36 +10,105 @@ bgcolor = input("Please enter the color of your background: ")
 
 ttcolor = input("Please enter the color of your turtle: ")
 
-turnspeed = input("Please enter how many units you would like the turtle to turn at a time: ")
 
-movespeed = input("Please enter how many units you would like the turtle to move at a time: ")
 
 # Setup the screen
 screen = turtle.Screen()
 screen.title("Etch A Sketch")
 screen.bgcolor(bgcolor)
-screen.setup(width=600, height=600)
+screen.setup(width=Width, height=Height)
 
 # Create the turtle
 etch = turtle.Turtle()
-etch.shape(ttcolor)
+etch.shape("turtle")
+etch.color(ttcolor)
 etch.speed(0)
 
 # Functions to move the turtle
 def move_forward():
-    etch.forward(int(movespeed))
+    etch.forward(10)
 
 def move_backward():
-    etch.backward(int(movespeed))
+    etch.backward(10)
 
 def turn_left():
-    etch.left(int(turnspeed))
+    etch.left(10)
 
 def turn_right():
-    etch.right(int(turnspeed))
+    etch.right(10)
 
 def clear_screen():
     etch.clear()
+
+def make_square():
+    for _ in range(4):
+        etch.forward(100)
+        etch.right(90)
+
+def make_hexagon():
+    for _ in range(6):
+        etch.forward(100)
+        etch.right(60)
+    
+
+def make_star():
+    for _ in range(5):
+        etch.forward(100)
+        etch.right(144)
+    
+    
+def make_flower():
+    for _ in range(12):
+        etch.right(30)
+        etch.forward(40)
+        etch.left(120)
+        etch.forward(40)
+        etch.left(120)
+        etch.forward(40)
+        etch.left(120)
+        etch.forward(40)
+
+def make_SEL():
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.right(90)
+    etch.forward(60)
+    etch.right(90)
+    etch.forward(60)
+    etch.right(180)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.right(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(120)
+    etch.right(90)
+    etch.forward(60)
+    etch.right(180)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.right(180)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(60)
+    etch.left(90)
+    etch.forward(120+60)
+    etch.right(180)
+    etch.forward(60)
+    etch.right(90)
+    etch.forward(120)
+
 
 # Keyboard bindings
 screen.listen()
@@ -47,7 +117,12 @@ screen.onkey(move_backward, "Down")
 screen.onkey(turn_left, "Left")
 screen.onkey(turn_right, "Right")
 screen.onkey(clear_screen, "c")
-screen.onkey(draw_circle, "1")
+screen.onkey(make_square, "q")
+screen.onkey(make_hexagon, "h")
+screen.onkey(make_star, "s")
+screen.onkey(make_flower, "f")
+screen.onkey(make_SEL, "e")
+screen.onkey(make_circle, "1")
 
 # Keep the window open
 turtle.done()
